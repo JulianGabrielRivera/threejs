ef005b96e350a3090cdc31a7ef43a3f5b99c3535
Julian Rivera <julianarevircoins@gmail.com>
2022-11-25 17:44:20 -0500

    text,clouds,particles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import { Canvas } from "@react-three/fiber";
 import Box from "./components/Box";
 import { OrbitControls } from "@react-three/drei";
 import AnimatedSphere from "./components/AnimatedSphere";
-// import {Text}
+import { Text } from "@react-three/drei";
 import { useRef } from "react";
-import { Sky, Sparkles, Cloud } from "@react-three/drei";
+import { Sky, Sparkles, Cloud, Stars } from "@react-three/drei";
 import boldUrl from "./fonts/firstFont.txt";
 // import { useFrame } from "@react-three/fiber";
+import { PresentationControls } from "@react-three/drei";
 
 import { Text3D } from "@react-three/drei";
 function App() {
@@ -43,17 +44,39 @@ function App() {
         />
         <Cloud
           opacity={0.5}
-          speed={0.4} // Rotation speed
+          speed={0.8} // Rotation speed
           width={10} // Width of the full cloud
-          depth={1.5} // Z-dir depth
+          depth={3} // Z-dir depth
           segments={20} // Number of particles
+        />
+        <Stars
+          radius={100}
+          depth={50}
+          count={5000}
+          factor={4}
+          saturation={0}
+          fade
+          speed={1}
         />
 
         {/* enables controls on the shape */}
         {/* <OrbitControls /> */}
-        <ambientLight intensity={0.5} />
-        <directionalLight position={(-2, 5, 2)} intensity={1} />
-        <Box></Box>
+        <PresentationControls
+          enabled={true} // the controls can be disabled by setting this to false
+          global={false} // Spin globally or by dragging the model
+          cursor={true} // Whether to toggle cursor style on drag
+          snap={true} // Snap-back to center (can also be a spring config)
+          speed={1} // Speed factor
+          zoom={3} // Zoom factor when half the polar-max is reached
+          rotation={[0, 0, 0]} // Default rotation
+          polar={[0, Math.PI / 2]} // Vertical limits
+          azimuth={[-Infinity, Infinity]} // Horizontal limits
+          config={{ mass: 1, tension: 170, friction: 90 }} // Spring config
+        >
+          <ambientLight intensity={0.5} />
+          <directionalLight position={(-2, 5, 2)} intensity={1} />
+          <Box></Box>
+        </PresentationControls>
       </Canvas>
       {/* <div className="skills">
         <Canvas className="canvas">
--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import { BoxBufferGeometry, MeshStandardMaterial } from "three";
 import { useLoader } from "@react-three/fiber";
 import { TextureLoader } from "three/src/loaders/TextureLoader";
-import texture from "../assets/flames.jpg";
+import texture from "../assets/vikingsFire.jpg";
 import { useFrame } from "@react-three/fiber";
 import { useRef } from "react";
 import { PerspectiveCamera } from "@react-three/drei";
 import { CubeTextureLoader } from "three";
 import { useCubeTexture } from "@react-three/drei";
 import { CubeCamera } from "@react-three/drei";
+import { Text } from "@react-three/drei";
+import { RenderTexture } from "@react-three/drei";
 
 const Box = () => {
   const profilePic = useLoader(TextureLoader, texture);
@@ -24,8 +26,23 @@ const Box = () => {
   return (
     <>
       <mesh rotation={[0, 0, 0]} ref={ref}>
-        <sphereGeometry attach="geometry" />
-        <meshStandardMaterial map={profilePic} />
+        <boxGeometry />
+        <meshStandardMaterial>
+          <RenderTexture attach="map" anisotropy={16}>
+            <PerspectiveCamera
+              makeDefault
+              manual
+              aspect={1 / 1}
+              position={[0, 0, 5]}
+            />
+
+            <ambientLight intensity={0.2} />
+            <directionalLight position={[10, 10, 5]} />
+            <Text color="white" fontSize={1}>
+              React
+            </Text>
+          </RenderTexture>
+        </meshStandardMaterial>
         <mesh
           rotation={[30, 30, 0]}
           ref={ref2}
